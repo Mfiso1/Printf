7e797a7f84cc0767cf426729641fd7f0bb969b9f
Mfiso <mfsdmnomezulu@gmail.com>
2023-07-23 04:43:47 -0700

    Preparing for Printf

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*The following section defines all 
+ *  standard header files utilized in the project*/ 
+
+#include <stdarg.h>
+#include <unistd.h>
+
+/* The following section declares prototypes utilized*/
+
+int _printf(const char *format, ...);
+int _putchar(char x);
+
+#endif/*MAIN_H*/
