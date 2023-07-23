851afeb96668abad329d1a4e52ef1438bb6b78a9
Mfiso <mfsdmnomezulu@gmail.com>
2023-07-23 06:16:14 -0700

    Betty bugs fixed on 0-functions file

--- 0-functions.c
+++ 0-functions.c
@@ -4,11 +4,11 @@
  * _putchar - writes the character x to stdout
  * @x: The character to print
  *
- * Returns: Upon success, 1 is returned
+ * Return: Upon success, 1 is returned
  * And upon error, -1 is returned
  */
 
 int _putchar(char x)
 {
-    return (write(1, &x, 1));
+	return (write(1, &x, 1));
 }
